1b5620adbc9135f5e643ad4fcaee5a2de42abaca
Levansarajishvili <120027976+Levansarajishvili@users.noreply.github.com>
2024-09-19 12:36:20 +0400

    treetree

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import TreeView from "./components/index";
+import Menus from "./components/data"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Tree view component/menu UI component / recursive navigation menu */}
+      <TreeView/>
     </div>
   );
 }
